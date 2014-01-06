9c544426f0b5db65852c712c9acc12e467aec19d
David Holm <dholmster@gmail.com>
2014-01-06 14:26:35 +0100

    Assign copyright to Steve Purcell
    Steve developed the original benchmark code and should be properly credited.

--- benchmark-init.el
+++ benchmark-init.el
@@ -1,34 +1,38 @@
 ;;; benchmark-init.el --- Benchmarks Emacs require and load calls
 
-;; Copyright (C) 2013  David Holm
+;; Copyright (C) 2013 Steve Purcell
+;; Copyright (C) 2013-2014 David Holm
 
-;; Author: David Holm <dholmster@gmail.com>
+;; Author: Steve Purcell
+;; Maintainer: David Holm <dholmster@gmail.com>
+;; Created: 25 Apr 2013
 ;; Keywords: benchmark
 
-;; This program is free software; you can redistribute it and/or modify
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
+;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
 ;; This is a simple benchmark of calls to Emacs require and load functions.
 ;; It can be used to keep track of where time is being spent during Emacs
 ;; startup in order to optimize startup times.
-;; The code was originally based on init-benchmarking.el by Steve Purcell but
-;; several modification has gone into it since.
+;; The code is based on init-benchmarking.el by Steve Purcell.
 
 ;;; Installation:
 
-;; Place this program in your load path and add the following code to the
+;; Place this file in your load path and add the following code to the
 ;; beginning of your Emacs initialization script.
 
 ;; (require 'benchmark-init)
