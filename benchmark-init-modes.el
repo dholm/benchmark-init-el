4b7d671ce67d41d90d826fa43c09006a2b8cbcdf
Makima0 <kekelanact@gmail.com>
2021-01-21 22:32:33 +0800

    Add version to define-obsolete-function-alias invocation

--- benchmark-init-modes.el
+++ benchmark-init-modes.el
@@ -209,7 +209,7 @@
 ;; Obsolete functions
 
 (define-obsolete-function-alias 'benchmark-init/show-durations
-  'benchmark-init/show-durations-tabulated)
+  'benchmark-init/show-durations-tabulated "1.0")
 
 (provide 'benchmark-init-modes)
 ;;; benchmark-init-modes.el ends here
